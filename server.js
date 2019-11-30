cbd62ca95091656ca8d4278b8ddd2905c4846794
kjfanj <kjfanj@gmail.com>
2019-11-30 12:14:49 -0500

    added working connection to aws rds with sample env file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
@@ -10,6 +11,18 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
+const { Client } = require('pg')
+
+const client = new Client({
+    user: process.env.RDS_USERNAME,
+    host: process.env.RDS_HOSTNAME,
+    database: process.env.RDS_DB_NAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT,
+})
+client.connect()
+    .then(() => console.log("connected to database successfully"))
+    .catch(err => console.log(err))
 
 const port = process.env.PORT || 5000;
 app.listen(port);
