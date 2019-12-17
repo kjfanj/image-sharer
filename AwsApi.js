3e59fc6909e45e586b60b3da05c5141787fa5130
kjfanj <kjfanj@gmail.com>
2019-12-17 01:54:28 -0500

    swapped cred to env

--- AwsApi.js
+++ AwsApi.js
@@ -1,11 +1,11 @@
 const uuidv4 = require('uuid/v4');
 const AWS = require('aws-sdk');
 
-// load aws config
-AWS.config.loadFromPath('./AwsConfig.json');
-const s3 = new AWS.S3();
-
 require('dotenv').config();
+
+process.env.AWS_SDK_LOAD_CONFIG = true;
+AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: process.env.AWS_S3_REGION });
+const s3 = new AWS.S3();
 
 module.exports = {
     // get presigned url for user to upload
